d475d9488ad6e665379896b5ed98799ae0d93f5f
pick001 <1753511170@qq.com>
2022-03-02 15:35:10 +0800

    项目初始化结构创建

---
+++ gulimail-master/src/main/java/com/atguigu/gulimail/master/GulimailMasterApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimail.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimailMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimailMasterApplication.class, args);
+    }
+
+}
---
+++ gulimail-master/src/test/java/com/atguigu/gulimail/master/GulimailMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimail.master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
