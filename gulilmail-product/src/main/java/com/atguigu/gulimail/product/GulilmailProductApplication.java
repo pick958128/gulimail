835e03f246ecf402903872eb84f62c4df648a25d
pick002 <394398474@qq.com>
2022-03-04 10:10:02 +0800

    Default Changelist

--- gulilmail-product/src/main/java/com/atguigu/gulimail/product/GulilmailProductApplication.java
+++ gulilmail-product/src/main/java/com/atguigu/gulimail/product/GulilmailProductApplication.java
@@ -1,8 +1,13 @@
 package com.atguigu.gulimail.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1、整合mybatis—plus
+ */
+@MapperScan("com/atguigu/gulimail/product/dao")
 @SpringBootApplication
 public class GulilmailProductApplication {
 
